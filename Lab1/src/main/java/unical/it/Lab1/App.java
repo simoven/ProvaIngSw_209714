fcbcefcfb4bb2cd40a725611610e00e2eeadfbdb
simoven <simo10@live.it>
2021-10-28 09:06:27 +0200

    FIrst commit

---
+++ Lab1/src/main/java/unical/it/Lab1/App.java
@@ -0,0 +1,13 @@
+package unical.it.Lab1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
