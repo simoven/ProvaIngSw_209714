b3a9a5c165a4c9afbd2764769f0fca7c6906edf8
simoven <simo10@live.it>
2021-10-28 10:15:53 +0200

    Done2

--- Lab1/src/main/java/unical/it/Lab1/App.java
+++ Lab1/src/main/java/unical/it/Lab1/App.java
@@ -1,13 +1,18 @@
 package unical.it.Lab1;
 
-/**
- * Hello world!
- *
- */
+import java.util.Arrays;
+import java.util.List;
+
+import unical.it.Lab1.MyListUtil.Order;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	List <Integer> list1 = Arrays.asList(3, 5, 9, 4, -1, 20, 10, 0);
+    	MyListUtil.sort(list1, Order.DECRESCENTE);
+    	
+    	//for(int num : list1)
+    	//	System.out.print(num + ", ");
     }
 }
